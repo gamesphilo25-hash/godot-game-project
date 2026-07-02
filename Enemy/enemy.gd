cb4980279cbf54d3de642149bb6dcdee22bc6956
gamesphilo25-hash <gamesphilo25@gmail.com>
2026-07-02 23:12:10 +0300

    godot project  video 3.1

---
+++ Enemy/enemy.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	
+	if body.is_in_group("plyer"):
+		var y_delta = position.y - body.position.y
+		print(y_delta)
+		if y_delta > 60:
+			print("destroy enemy")
+		else:
+			print("decrease plyer health")
